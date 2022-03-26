dc0f34e269b7e9b193a9bdc0f9160a1d414add4b
Lien Zhu <lienzhucs@gmail.com>
2022-03-25 20:42:56 -0500

    saving

---
+++ image-gallery.js
@@ -0,0 +1,25 @@
+// script file for image gallery assignment 7
+//
+
+// start with array of image urls
+// empty page on start, function displayImagesFromArray(img array)
+// that iterates through array and displays them
+// for each url create a div to hold the image, the img itself
+//
+
+// make image big on click
+// 1. make a hidden div called big-div
+// display: none, on click put image into big-div, set display: block
+// function makeImageBig(some identifier of image)
+//
+
+// function closeBigImage()
+// set big-div display: none;
+//
+
+// left right arrows
+// function nextImageBig(current img)
+// call closeBigImage() on current image
+// figure out next image, call makeImageBig(next image)
+//
+
