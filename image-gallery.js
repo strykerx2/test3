2f6d7ec1da4169819fa6b286e92bf5860665b150
Lien Zhu <lienzhucs@gmail.com>
2022-03-25 23:30:19 -0500

    imgInit() works well, but need to arrange images in grid, enlargeImage() just displays clicked image at top, so need to figure out how to display over other images, closeBigImage() works well

--- image-gallery.js
+++ image-gallery.js
@@ -1,6 +1,11 @@
 // script file for image gallery assignment 7
 //
 
+
+
+// set global elements
+//
+
 let imgArray = [ "img/atl.png", "img/bos.png", "img/chh.png", "img/chi.png", "img/cle.png", "img/gsw.png", "img/hou.png", "img/lal.png", "img/mia.png", "img/okc.png", "img/phx.png", "img/tor.png" ]
 
 let bigDiv = document.createElement("div");
@@ -21,11 +26,10 @@ closeBtn.addEventListener("click", closeBigImage);
 bigDiv.appendChild(closeBtn);
 bigDiv.appendChild(bigImg);
 
+
+
 // function imgInit(img array)
-// start with array of image urls
-// empty page on start
-// that iterates through array and displays them
-// for each url create a div to hold the image, the img itself
+// takes in array of image urls, wraps each in a div
 //
 function imgInit(arr) {
 
@@ -49,9 +53,7 @@ function imgInit(arr) {
 
 
 // function enlargeImage(class or id of image)
-// make image big on click
-// make a bigDiv div called big-div
-// display: none, on click put image into big-div, set display: block
+// changes link of bigImg <img> that is inside the hidden bigDiv
 //
 function enlargeImage(id) {
     console.log("enlargeImage() called on " + id);
@@ -63,8 +65,6 @@ function enlargeImage(id) {
 
     bigDiv.style.display = "block";
 }
-
-
 
 
 // function closeBigImage()
